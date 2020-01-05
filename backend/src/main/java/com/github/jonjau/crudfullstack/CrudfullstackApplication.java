0d01eb59a5d09297b8b9b18296ee8ba248fb0f42
jonjau <jonjau3Q@gmail.com>
2020-01-05 16:34:23 +0700

    bootstrapped Spring backend

---
+++ backend/src/main/java/com/github/jonjau/crudfullstack/CrudfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonjau.crudfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfullstackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/jonjau/crudfullstack/CrudfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonjau.crudfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
